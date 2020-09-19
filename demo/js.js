e4a4fcc8666e67858ecc8d72a7ed3b2cbb0be7be
Magnus Nord <36410135+NordMagnus@users.noreply.github.com>
2020-09-19 19:15:50 +0200

    Added some random code snippets

--- demo/js.js
+++ demo/js.js
@@ -8,10 +8,43 @@ const joe = function () { };
 
 const wes = 100;
 
-function wes() {
+function wesFunc() {
 
 }
 
-const wes = () => { };
+console.log('test');
+
+function anotherFunc() {
+  let variable = null;
+  const aConstant = "test";
+  const internalFunc = () => {
+    if (a < 32 && b === 0) {
+      // This is a comment
+      doStuff();
+    }
+  }
+  return "good";
+}
+
+anotherFunc.internalFunc();
+anotherFunc.variable.test();
+
+const arrowWes = () => { };
 
 console.log(joe, wes);
+
+
+class MyClass extends BaseClass {
+
+  constructor() {
+    this._privateVar = 43;
+  }
+
+  methodA(param1) {
+    return param1 ** 2;
+  }
+
+  methodB(param1, param2) {
+    return this.methodA(param1 > param2 ? param1 : param2);
+  }
+}
